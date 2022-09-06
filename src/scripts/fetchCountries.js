354c4d0c596432112289c5eaf7956c692cde2192
Ivan Chornyi <chorniyivan@icloud.com>
2022-09-06 18:23:43 +0300

    404 error fixed

--- src/scripts/fetchCountries.js
+++ src/scripts/fetchCountries.js
@@ -6,7 +6,12 @@ export const countrySearch = {
     
     fetchCountries() {
     return fetch(`${this.url}/v3.1/name/${this.toFind}?fields=${this.parameters.join(',')}`)
-        .then(response => response.json());
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
     },
 
 };
